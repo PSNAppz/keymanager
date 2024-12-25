95faafd3f8bf81ec02a44515f477a9ea5650cb7a
PS Narayanan <thepsnarayanan@gmail.com>
2024-12-25 11:41:01 +0530

    Update configure_start.sh
    Signed-off-by: PS Narayanan <thepsnarayanan@gmail.com>

--- kernel/kernel-keymanager-service/configure_start.sh
+++ kernel/kernel-keymanager-service/configure_start.sh
@@ -3,9 +3,9 @@
 #installs the pkcs11 libraries.
 set -e
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get install libc6-dev=2.34-0ubuntu0.21.10.1 -y 
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt-get update
+apt-get install libc6-dev=2.34-0ubuntu0.21.10.1 -y 
 
 DEFAULT_ZIP_PATH=artifactory/libs-release-local/hsm/client.zip
 [ -z "$hsm_zip_file_path" ] && zip_path="$DEFAULT_ZIP_PATH" || zip_path="$hsm_zip_file_path"
